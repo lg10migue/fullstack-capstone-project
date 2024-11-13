39c9659b41463d1c59bcc3d77b1d330aa049aab9
Miguel Angel López Guzmán <lg10migue@gmail.com>
2024-11-12 18:48:13 -0700

    Update Navbar component.

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from "react";
 
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">GiftLink</a>
-
+            <a className="navbar-brand" href="/">
+                GiftLink
+            </a>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     {/* Task 1: Add links to Home and Gifts below*/}
+                    <li className="nav-item">
+                        <a href="/home.html" className="nav-link">
+                            Home
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/app" className="nav-link">
+                            Gifts
+                        </a>
+                    </li>
                 </ul>
             </div>
         </nav>
