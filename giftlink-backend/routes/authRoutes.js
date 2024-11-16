872daabf3b1d9a9631ed63950e28ed40583c3b38
Miguel Angel López Guzmán <lg10migue@gmail.com>
2024-11-16 13:31:54 -0700

    Develop back-end integration for registration.

---
+++ giftlink-backend/routes/authRoutes.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const bcryptsjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
+const connectToDatabase = require("../models/db");
+const router = express.Router();
+const dotenv = require("dotenv");
+const pino = require("pino");
+
+// Create a Pino logger instance.
+const logger = pino();
+
+dotenv.config();
+
+// Create JWT secret.
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post("/register", async (req, res) => {
+    try {
+        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+
+        // Task 2: Access MongoDB collection.
+        const collection = db.collection("users");
+
+        //Task 3: Check for existing email.
+        const existingEmail = await collection.findOne({
+            email: req.body.email,
+        });
+
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(req.body.password, salt);
+        const email = req.body.email;
+
+        // Task 4: Save user details in database.
+        const newUser = await collection.insertOne({
+            email: email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
+
+        //Task 5: Create JWT authentication with user._id as payload.
+        const payload = {
+            user: {
+                id: newUser.insertedId,
+            },
+        };
+
+        const authToken = jwt.sign(payload, JWT_SECRET);
+        logger.info("User registered successfully");
+        res.json({ authToken, email });
+    } catch (e) {
+        return res.status(500).send("Internal server error");
+    }
+});
+
+module.exports = router;
