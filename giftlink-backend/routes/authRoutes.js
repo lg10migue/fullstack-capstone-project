9bf8dc767dd8516c80ce5d063afd36e038cc2190
Miguel Angel López Guzmán <lg10migue@gmail.com>
2024-11-16 23:23:42 -0700

    Develop back-end integration for login.

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const app = express();
-const bcryptsjs = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { body, validationResult } = require("express-validator");
 const connectToDatabase = require("../models/db");
 const router = express.Router();
 const dotenv = require("dotenv");
@@ -29,6 +27,11 @@ router.post("/register", async (req, res) => {
             email: req.body.email,
         });
 
+        if (existingEmail) {
+            logger.error("Email already exists");
+            return res.status(400).json({ error: "Email already exists" });
+        }
+
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email = req.body.email;
@@ -54,6 +57,53 @@ router.post("/register", async (req, res) => {
         res.json({ authToken, email });
     } catch (e) {
         return res.status(500).send("Internal server error");
+    }
+});
+
+router.post("/login", async (req, res) => {
+    console.log("\n\n Inside login");
+
+    try {
+        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+
+        // Task 2: Access MongoDB `users` collection.
+        const collection = db.collection("users");
+
+        // Task 3: Check for user credentials in database.
+        const user = await collection.findOne({ email: req.body.email });
+
+        // Task 4: Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch.
+        if (user) {
+            let result = await bcryptjs.compare(
+                req.body.password,
+                user.password
+            );
+            if (!result) {
+                logger.error("Passwords do not match");
+                return res.status(404).json({ error: "Wrong password" });
+            }
+
+            let payload = { user: { id: user._id.toString() } };
+
+            // Task 5: Fetch user details from database.
+            const userName = user.firstName;
+            const userEmail = user.email;
+
+            // Task 6: Create JWT authentication if passwords match with user._id as payload.
+            const authToken = jwt.sign(payload, JWT_SECRET);
+            logger.info("User logged in successfully");
+            return res.status(200).json({ authToken, userName, userEmail });
+        } else {
+            // Task 7: Send appropriate message if user not found.
+            logger.error("User not found");
+            return res.status(404).json({ error: "User not found" });
+        }
+    } catch (e) {
+        logger.error(e);
+        return res
+            .status(500)
+            .json({ error: "Internal server error", details: e.message });
     }
 });
 
